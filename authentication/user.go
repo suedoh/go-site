8bf4fdccd804e5a77e4c97b2bfcbc2b07d3df723
suedoh <suedoh36@proton.me>
2023-01-23 19:15:31 -0500

    user struct, might remove or change but for now it's isolated

--- authentication/user.go
+++ authentication/user.go
@@ -2,7 +2,8 @@ package authentication
 
 // A User represents a basic user of the system
 type User struct {
-    Username string
-    Password string
+    ID       int    `json:"id"`
+    Username string `json:"username"`
+    Password []byte `json:"password"`
 }
 
