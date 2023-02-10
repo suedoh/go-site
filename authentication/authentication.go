e0d7825051187c449b0338eba287a9ab0cf26b6b
suedoh <suedoh36@proton.me>
2023-02-09 23:13:11 -0500

    adding csrf and jwt logic

--- authentication/authentication.go
+++ authentication/authentication.go
@@ -1,52 +1,81 @@
 package authentication
 
 import (
+	"errors"
 	"net/http"
+	"time"
 
-	"github.com/suedoh/go-site/authentication/auth"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/csrf"
 )
 
-// LoginHandler handles the login route
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	// Call the Login function in auth package
-	err := auth.Login(w, r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Redirect to protected page
-	http.Redirect(w, r, "/protected", http.StatusFound)
+// NewAuth returns a new Auth middleware with the given config
+func NewAuth(config Config) *Auth {
+	return &Auth{config}
 }
 
-// LogoutHandler handles the logout route
-func LogoutHandler(w http.ResponseWriter, r *http.Request) {
-	// Call the Logout function in auth package
-	err := auth.Logout(w, r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+// AuthMiddleware is a middleware that validates the JWT token
+func (a *Auth) AuthMiddleware(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		// get the cookie
+		c, err := r.Cookie("token")
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
 
-	// Redirect to home page
-	http.Redirect(w, r, "/", http.StatusFound)
+		// validate the token
+		claims, err := a.ValidateJWT(c.Value)
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// set the claims in the request context
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, claimsKey, claims)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+	return http.HandlerFunc(fn)
 }
 
-// ProtectedHandler handles the protected route
-func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
-	// Call the ValidateJWT function in auth package
-	valid, err := auth.ValidateJWT(r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+func (a *Auth) Login(w http.ResponseWriter, r *http.Request) {
+    var user User
 
-	if !valid {
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
+    if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+        http.Error(w, "Error decoding JSON", http.StatusBadRequest)
+        return
+    }
 
-	// Render the protected page
-	w.Write([]byte("You have successfully logged in!"))
+    if !a.checkUser(user) {
+        http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+        return
+    }
+
+    // create JWT token
+    token, err := a.createJWT(user)
+    if err != nil {
+        http.Error(w, "Error creating JWT", http.StatusInternalServerError)
+        return
+    }
+
+    http.SetCookie(w, &http.Cookie{
+        Name:    "token",
+        Value:   token,
+        Expires: time.Now().Add(time.Minute * 15),
+    })
+}
+
+func (a *Auth) Logout(w http.ResponseWriter, r *http.Request) {
+    // Clear the cookie
+    http.SetCookie(w, &http.Cookie{
+        Name:     "token",
+        Value:    "",
+        Expires:  time.Now(),
+        HttpOnly: true,
+    })
+
+    // Redirect to the login page
+    http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
